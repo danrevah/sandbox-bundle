3127f32194c3efc049f6061a58eb9e84e9a0d8d5
Dan Revah <danrevah89@gmail.com>
2015-02-20 02:00:47 +0200

    configuration and appkernel fixes in test

--- Tests/KernelAwareTest.php
+++ Tests/KernelAwareTest.php
@@ -1,14 +1,12 @@
 <?php
 
-require_once 'AppKernel.php';
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
  * Test case class helpful with Entity tests requiring the database interaction.
  * For regular entity tests it's better to extend standard \PHPUnit_Framework_TestCase instead.
- *
- * Taken from https://gist.github.com/jakzal/1319290
  */
-abstract class KernelAwareTest extends \PHPUnit_Framework_TestCase
+abstract class KernelAwareTest extends WebTestCase
 {
     protected static $kernel;
 
@@ -27,42 +25,19 @@ abstract class KernelAwareTest extends \PHPUnit_Framework_TestCase
      */
     public static function setUpBeforeClass()
     {
-        self::$kernel = new \AppKernel('sandbox', true);
+        self::$kernel = static::createKernel(['environment' => 'sandbox']);
         self::$kernel->boot();
-
-        self::$container = self::$kernel->getContainer();
-        self::$entityManager = self::$container->get('doctrine')->getManager();
-
-        self::generateSchema();
+        self::$entityManager = self::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
     }
 
     /**
      * @return null
      */
-    public function tearDown()
+    public static function tearDownAfterClass()
     {
-        self::$kernel->shutdown();
+        self::$entityManager->close();
     }
 
-    /**
-     * @return null
-     */
-    protected function generateSchema()
-    {
-        $metadatas = self::getMetadatas();
-
-        if (!empty($metadatas)) {
-            $tool = new \Doctrine\ORM\Tools\SchemaTool(self::$entityManager);
-            $tool->dropSchema($metadatas);
-            $tool->createSchema($metadatas);
-        }
-    }
-
-    /**
-     * @return array
-     */
-    protected function getMetadatas()
-    {
-        return self::$entityManager->getMetadataFactory()->getAllMetadata();
-    }
 }
