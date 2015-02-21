ca65f57cb9b9545408ca1cfe7b53429eb9111aef
Dan Revah <danrevah89@gmail.com>
2015-02-22 01:41:21 +0200

    boostrap

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,11 +1,13 @@
 <?php
 
-$includeIfExists = function($file)
+function includeIfExists($file)
 {
-    return file_exists($file) ? include $file : false;
-};
+    if (file_exists($file)) {
+        return include $file;
+    }
+}
 
-if (( ! $loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && ( ! $loader = includeIfExists(__DIR__.'/../../../../../autoload.php'))) {
+if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../../../autoload.php'))) {
     die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
         'php composer.phar install'.PHP_EOL);
